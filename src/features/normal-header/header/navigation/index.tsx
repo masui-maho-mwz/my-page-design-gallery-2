b41398e35cc4ea09c14b62f41c2fa8cd0df02088
masui-maho-mwz <m.masui@miwaza.co.jp>
2024-06-06 12:29:24 +0900

    create: normal-hader/header/navigation

--- src/features/normal-header/header/navigation/index.tsx
+++ src/features/normal-header/header/navigation/index.tsx
@@ -1,5 +1,25 @@
+import Link from 'next/link';
 import styles from './styles.module.css';
 
-export const Navigation = () => {
-  return <div className={styles.root}>Navigation</div>;
+type Item = {
+  id: number;
+  label: string;
+  href: string;
+  isNew?: boolean;
+};
+type Props = {
+  items: Item[];
+};
+
+export const Navigation = ({ items }: Props) => {
+  return (
+    <nav className={styles.root}>
+      {items.map(({ id, label, href, isNew }) => (
+        <Link key={id} href={href} className={styles.item}>
+          {label}
+          {isNew && <span className={styles.chip}>NEW</span>}
+        </Link>
+      ))}
+    </nav>
+  );
 };
--- src/features/normal-header/header/page.tsx
+++ src/features/normal-header/header/page.tsx
@@ -6,7 +6,16 @@ export const Header = () => {
   return (
     <div className={styles.root}>
       <TopHeader />
-      <Navigation />
+      <Navigation items={items} />
     </div>
   );
 };
+
+const items = [
+  { id: 1, label: 'nav1', href: '#' },
+  { id: 2, label: 'nav2', href: '#' },
+  { id: 3, label: 'nav3', href: '#' },
+  { id: 4, label: 'nav4', href: '#' },
+  { id: 5, label: 'nav5', href: '#', isNew: true },
+  { id: 6, label: 'nav6', href: '#' },
+];
