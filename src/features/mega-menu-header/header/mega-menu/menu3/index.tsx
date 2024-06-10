3b2efe20e6d1f102ea8448987c9323224774d54e
masui-maho-mwz <m.masui@miwaza.co.jp>
2024-06-10 17:12:55 +0900

    fix: mega-menu-header/header/menu-list を追加

--- src/features/mega-menu-header/header/mega-menu/menu3/index.tsx
+++ src/features/mega-menu-header/header/mega-menu/menu3/index.tsx
@@ -1,5 +1,18 @@
+import { MenuListContainer } from '@/features/mega-menu-header/header/mega-menu/menu-list-container';
+import {
+  categories,
+  others,
+  services1,
+  services2,
+} from '@/features/mega-menu-header/header/mega-menu/menu3/menu3-data';
 import styles from './styles.module.css';
 
 export const Menu3 = () => {
-  return <div className={styles.root}>menu3</div>;
+  return (
+    <div className={styles.root}>
+      <MenuListContainer title="サービス一覧" items={[services1, services2]} width={450} showBorderRight={true} />
+      <MenuListContainer title="カテゴリ一覧" items={[categories]} width={225} showBorderRight={true} />
+      <MenuListContainer title="その他" items={[others]} width={225} showBorderRight={false} />
+    </div>
+  );
 };
