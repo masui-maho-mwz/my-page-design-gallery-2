f0f7caa516802d003f1655bf74fec03a51e91423
masui-maho-mwz <m.masui@miwaza.co.jp>
2024-06-11 12:43:16 +0900

    fix: dashboard/header/style

---
+++ src/components/dashboard-menu/elements/buttons/icon-button/index.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import styles from './styles.module.css';
+
+type Props = {
+  icon: React.ReactNode;
+  onClick?: (e: React.MouseEvent<HTMLButtonElement>) => void;
+  disabled?: boolean;
+};
+
+export const IconButton = ({ icon, onClick, disabled = false }: Props) => {
+  return (
+    <button className={styles.root} onClick={onClick} disabled={disabled}>
+      {icon}
+    </button>
+  );
+};
+
+// export default IconButton;
--- src/features/dashboard-menu/header/index.tsx
+++ src/features/dashboard-menu/header/index.tsx
@@ -1,9 +1,22 @@
+import { IconButton } from '@/components/dashboard-menu/elements/buttons/icon-button';
+import { Bell } from '@/components/dashboard-menu/icons/bell';
+import { Loupe } from '@/components/dashboard-menu/icons/loupe';
+import { UnionJack } from '@/components/dashboard-menu/icons/union-jack';
 import styles from './styles.module.css';
 
 export const Header = () => {
   return (
     <header className={styles.root}>
-      <div>header</div>
+      <div className={styles.wrap}>
+        <button className={styles.button}>
+          <Loupe />
+        </button>
+        <div className={styles.icons}>
+          <IconButton icon={<UnionJack />} />
+          <IconButton icon={<Bell />} />
+          <IconButton icon={<div className={styles.icon}>icon</div>} />
+        </div>
+      </div>
     </header>
   );
 };
