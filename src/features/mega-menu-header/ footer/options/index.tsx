0a0bfe276440e5216df77b6f307a06546891ac17
masui-maho-mwz <m.masui@miwaza.co.jp>
2024-06-11 09:32:09 +0900

    add: mega-menu-header/footer

--- src/features/mega-menu-header/ footer/options/index.tsx
+++ src/features/mega-menu-header/ footer/options/index.tsx
@@ -1,3 +1,7 @@
+import { AbstractLogo } from '@/components/mega-menu-header/logos/teq-quest-logos';
+import Link from 'next/link';
+import styles from './styles.module.css';
+
 type Link = {
   id: number;
   url: string;
@@ -8,7 +12,24 @@ type Props = {
   links: Link[];
 };
 
-// TODO: ここから再開
 export const Options = ({ links }: Props) => {
-  return <div>options</div>;
+  return (
+    <div className={styles.root}>
+      <div className={styles.wrap}>
+        <div className={styles.logo}>
+          <AbstractLogo />
+        </div>
+        <small>Copyright ©xxxx Corporation. All rights reserved.</small>
+      </div>
+      <ul className={styles.lists}>
+        {links.map(({ id, url, text }) => (
+          <li key={id} className={styles.list}>
+            <Link href={url} className={styles.link}>
+              {text}
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 };
