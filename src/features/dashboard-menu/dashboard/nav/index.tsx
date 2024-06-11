4b0d1b4107f50c16559729136d8464bc568192f2
masui-maho-mwz <m.masui@miwaza.co.jp>
2024-06-11 11:14:47 +0900

    fix, add: dashboard/nav/style, state

--- src/features/dashboard-menu/dashboard/nav/index.tsx
+++ src/features/dashboard-menu/dashboard/nav/index.tsx
@@ -1,4 +1,7 @@
+'use client';
+
 import Link from 'next/link';
+import { useState } from 'react';
 import styles from './styles.module.css';
 
 type Item = {
@@ -12,10 +15,21 @@ type Props = {
 };
 
 export const Nav = ({ items }: Props) => {
+  const [activeIndex, setActiveIndex] = useState(1);
+
+  const handleClick = (id: number) => {
+    setActiveIndex(id);
+  };
+
   return (
     <nav className={styles.root}>
       {items.map(({ id, label, href }) => (
-        <Link key={id} href={href} className={styles.link}>
+        <Link
+          key={id}
+          href={href}
+          className={`${styles.link} ${id === activeIndex ? styles.active : ''}`}
+          onClick={() => handleClick(id)}
+        >
           <div className={styles.label}>{label}</div>
         </Link>
       ))}
